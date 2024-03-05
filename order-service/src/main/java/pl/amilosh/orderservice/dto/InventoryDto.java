8fc884176b851fb0b1d5e94fe7e160acd52be858
Alexander Milosh <milosh.alexander.by@gmail.com>
2024-03-05 21:24:03 +0100

    WebFlux WebClient, order-service to inventory-service

---
+++ order-service/src/main/java/pl/amilosh/orderservice/dto/InventoryDto.java
@@ -0,0 +1,17 @@
+package pl.amilosh.orderservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class InventoryDto {
+
+    private Integer id;
+    private String skuCode;
+    private Integer quantity;
+}
--- order-service/src/main/java/pl/amilosh/orderservice/service/impl/OrderServiceImpl.java
+++ order-service/src/main/java/pl/amilosh/orderservice/service/impl/OrderServiceImpl.java
@@ -2,12 +2,21 @@ package pl.amilosh.orderservice.service.impl;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
+import pl.amilosh.orderservice.dto.InventoryDto;
 import pl.amilosh.orderservice.dto.OrderDto;
 import pl.amilosh.orderservice.mapping.OrderMapper;
+import pl.amilosh.orderservice.model.Order;
+import pl.amilosh.orderservice.model.OrderLineItem;
 import pl.amilosh.orderservice.repository.OrderRepository;
 import pl.amilosh.orderservice.service.OrderService;
+
+import java.util.Arrays;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON;
 
 @Slf4j
 @Service
@@ -17,12 +26,44 @@ public class OrderServiceImpl implements OrderService {
 
     private final OrderMapper orderMapper;
     private final OrderRepository orderRepository;
+    private final WebClient webClient;
 
     @Override
     public OrderDto createOrder(OrderDto orderDto) {
         log.info("Create order");
         var order = orderMapper.toEntity(orderDto);
+        validateOrder(order);
         var savedOrder = orderRepository.save(order);
         return orderMapper.toDto(savedOrder);
+    }
+
+    private void validateOrder(Order order) {
+        var orderSkus = order.getOrderLineItems().stream()
+            .map(OrderLineItem::getSkuCode).toList();
+
+        var inventories = webClient.get()
+            .uri("http://inventory-service:8081/inventory",
+                uriBuilder -> uriBuilder.queryParam("skuCode", orderSkus).build())
+            .accept(APPLICATION_JSON)
+            .retrieve()
+            .bodyToMono(InventoryDto[].class).block();
+
+        if (inventories == null) {
+            throw new IllegalArgumentException("User placed order with item that doesn't exist in inventory store");
+        }
+
+        var inventoriesSkus = Arrays.stream(inventories)
+            .map(InventoryDto::getSkuCode).toList();
+
+        if (!CollectionUtils.isEqualCollection(orderSkus, inventoriesSkus)) {
+            throw new IllegalArgumentException("User placed order with item that doesn't exist in inventory store");
+        }
+
+        var itemWithoutInventoryExists = Arrays.stream(inventories)
+            .anyMatch(inventory -> inventory.getQuantity() == 0);
+
+        if (itemWithoutInventoryExists) {
+            throw new IllegalArgumentException("User placed order with item that has quantity 0");
+        }
     }
 }
